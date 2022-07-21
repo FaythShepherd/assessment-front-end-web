c159612d32fc54ee5985ccadbc58422fb85743fe
Fayth Shepherd <faythshep@gmail.com>
2022-07-21 17:42:28 -0600

    first commit

--- about.js
+++ about.js
@@ -18,3 +18,13 @@ let cuteCat = document.querySelector('img')
 cuteCat.addEventListener('mouseover', (evt) => {
 	alert('You are awesome!')
 })
+
+/* Toggle between adding and removing the "responsive" class to topnav when the user clicks on the icon */
+function myFunction() {
+	var x = document.getElementById("myTopnav");
+	if (x.className === "topnav") {
+	  x.className += " responsive";
+	} else {
+	  x.className = "topnav";
+	}
+  }
